03cb5fa865e72c73685aaccb9d34837854f6893d
Freesoucesprofessional <githubofficial565@gmail.com>
2026-03-31 10:18:33 +0530

    live user etc added

--- src/pages/Reseller.jsx
+++ src/pages/Reseller.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import gsap from 'gsap';
 import {
